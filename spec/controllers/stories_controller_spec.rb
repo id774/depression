deb6d99e07062ba21d7f6fdb4e08cbea93502457
id774 <idnanashi@gmail.com>
2014-01-06 05:23:30 +0900

    17 Append test cases

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -6,7 +6,6 @@ describe StoriesController, 'Stories' do
   fixtures :all
 
   context 'にアクセスする場合' do
-    # login_admin
 
     def create_post(string)
       post 'create' , :story => {
@@ -43,6 +42,11 @@ describe StoriesController, 'Stories' do
         response.body.should have_at_least(1).items
         flash[:notice].should_not be_nil
         flash[:notice].should eq '判定結果は「鬱ツイート」です'
+        session[:result].should eq '判定結果は「鬱ツイート」です'
+        session[:text].should eq "死死死"
+        session[:classify].should eq "鬱ツイート"
+        session[:total_score].should eq -0.2041627108542056
+        session[:scores].should eq "[[\"死\",\"し\",\"名詞\",\"-0.588833\"],[\"死\",\"し\",\"名詞\",\"-0.588833\"],[\"死\",\"し\",\"名詞\",\"-0.588833\"]]"
       end
 
       it "新規レコードが作成される" do
@@ -51,6 +55,7 @@ describe StoriesController, 'Stories' do
         content.text.should eq "死死死"
         content.classify.should eq "鬱ツイート"
         content.total_score.should <= -0.20
+        Story.count.should eq 2
       end
     end
 
@@ -62,6 +67,11 @@ describe StoriesController, 'Stories' do
         response.body.should have_at_least(1).items
         flash[:notice].should_not be_nil
         flash[:notice].should eq '判定結果は「普通のツイート」です'
+        session[:result].should eq '判定結果は「普通のツイート」です'
+        session[:text].should eq "元気に生きる"
+        session[:classify].should eq "普通のツイート"
+        session[:total_score].should eq 0.8873056004837514
+        session[:scores].should eq "[[\"元気\",\"げんき\",\"名詞\",\"0.993766\"],[\"生きる\",\"いきる\",\"動詞\",\"0.928091\"]]"
       end
 
       it "新規レコードが作成される" do
@@ -70,6 +80,7 @@ describe StoriesController, 'Stories' do
         content.text.should eq "元気に生きる"
         content.classify.should eq "普通のツイート"
         content.total_score.should > -0.20
+        Story.count.should eq 2
       end
     end
 
@@ -81,6 +92,11 @@ describe StoriesController, 'Stories' do
         response.body.should have_at_least(1).items
         flash[:notice].should_not be_nil
         flash[:notice].should eq '判定結果は「判定不能」です'
+        session[:result].should eq '判定結果は「判定不能」です'
+        session[:text].should eq "元気モリモリ"
+        session[:classify].should eq "判定不能"
+        session[:total_score].should eq 0.0
+        session[:scores].should eq "語彙が少なすぎて判定できません。もう少し語彙を増やしてください。"
       end
 
       it "新規レコードが作成される" do
@@ -89,6 +105,7 @@ describe StoriesController, 'Stories' do
         content.text.should eq "元気モリモリ"
         content.classify.should eq "判定不能"
         content.total_score.should eq 0.0
+        Story.count.should eq 2
       end
     end
 
